b77de8ba3dbdebb09a415027e094e3403eb5b1ea
Paul Popat <paul@popatux.co.uk>
2022-10-26 09:39:49 +0100

    Accepted buffer response types

--- response-applier.ts
+++ response-applier.ts
@@ -1,8 +1,23 @@
 import { IsString } from "./deps.ts";
 import { Response as PureResponse } from "./server.ts";
 
+const AcceptedTypes = [
+  Blob,
+  ArrayBuffer,
+  FormData,
+  URLSearchParams,
+  ReadableStream<Uint8Array>,
+];
+
 export default function Send(response: PureResponse): Response {
   const original_body = response.body;
+  for (const type of AcceptedTypes)
+    if (original_body instanceof type)
+      return new Response(original_body, {
+        status: response.status,
+        headers: response.headers,
+      });
+
   const body = IsString(original_body)
     ? original_body
     : JSON.stringify(original_body);

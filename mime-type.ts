55f3933a5a7dc8a020595583b61cda2551bef3b9
Paul Popat <paul@popatux.co.uk>
2022-10-26 11:24:58 +0100

    Allowed form encoded requests

--- mime-type.ts
+++ mime-type.ts
@@ -4,6 +4,7 @@ export function ParseMimeType(header_value: string) {
   if (data.startsWith("text/")) return "text" as const;
   if (data === "application/json") return "json" as const;
   if (data === "multipart/form-data") return "formData" as const;
+  if (data === "application/x-www-form-urlencoded") return "formData" as const;
   if (data.startsWith("image/")) return "blob" as const;
   return "arrayBuffer";
 }

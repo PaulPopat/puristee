b250fa52d6a1543ac33de572db6e5aa8e192af1d
Paul Popat <paul@popatux.co.uk>
2023-03-15 09:01:19 +0000

    Updated fs_db

--- deps.ts
+++ deps.ts
@@ -1,5 +1,5 @@
 export * from "https://deno.land/x/type_guard@2.3.1/index.ts";
-export * from "https://deno.land/x/fs_db@2.0.2/mod.ts";
+export * from "https://deno.land/x/fs_db@2.0.3/mod.ts";
 export { deepmerge as DeepMerge } from "https://deno.land/x/deepmergets@v4.2.2/dist/deno/index.ts";
 export * as MediaType from "https://deno.land/std@0.161.0/media_types/mod.ts";
 export * as Path from "https://deno.land/std@0.161.0/path/mod.ts";
